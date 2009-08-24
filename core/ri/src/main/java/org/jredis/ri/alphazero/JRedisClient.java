4c8e1503f71c7ba5c33b96c0b71b38ee450548fd
Joubin Houshyar <alphazero@sensesay.net>
2009-08-23 21:46:50 -0400

    Source update only: It probably helps if the new static methods are public ...

--- core/ri/src/main/java/org/jredis/ri/alphazero/JRedisClient.java
+++ core/ri/src/main/java/org/jredis/ri/alphazero/JRedisClient.java
@@ -252,7 +252,7 @@ public class JRedisClient extends SynchJRedisBase  {
 	/**
 	 * @return the {@link ConnectionSpec} used by default by this {@link JRedisClient}
 	 */
-	ConnectionSpec getDefaultConnectionSpec () {
+	public ConnectionSpec getDefaultConnectionSpec () {
         ConnectionSpec defaultConnectionSpec = null;
 		try {
 			defaultConnectionSpec = getDefaultConnectionSpec("localhost", 6379, null, 0);
@@ -270,7 +270,7 @@ public class JRedisClient extends SynchJRedisBase  {
 	 * @return the {@link ConnectionSpec} used by default by this {@link JRedisClient} for the given params.
      * @throws UnknownHostException
      */
-    private ConnectionSpec getDefaultConnectionSpec (String host, int port, String password, int database) throws UnknownHostException {
+    public ConnectionSpec getDefaultConnectionSpec (String host, int port, String password, int database) throws UnknownHostException {
     	InetAddress address = InetAddress.getByName(host);
 	    return SynchConnection.getDefaultConnectionSpec(address, port, database, getCredentialBytes(password));
     }
