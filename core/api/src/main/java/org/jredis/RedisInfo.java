afdbc47632b637e98815df1a9639f6388e2d73b3
Joubin Houshyar <alphazero@sensesay.net>
2010-03-23 15:24:20 -0400

    Updated RedisInfo.  Tested.

--- core/api/src/main/java/org/jredis/RedisInfo.java
+++ core/api/src/main/java/org/jredis/RedisInfo.java
@@ -18,21 +18,37 @@ package org.jredis;
 
 /**
  * [TODO: document me!]
+ * [TODO: dbN:keys=2,expires=0 - where N: {0->n}]
+
  *
  * @author  Joubin Houshyar (alphazero@sensesay.net)
  * @version alpha.0, 04/02/09
  * @since   alpha.0
  * 
  */
+/*
+ * TOOD:
+ */
 public enum RedisInfo {
+	arch_bits,
+	multiplexing_api,
 	redis_version,
+	process_id,
 	connected_clients,
 	connected_slaves,
+	blocked_clients,
 	used_memory,
+	used_memory_human,
 	changes_since_last_save,
+	bgsave_in_progress,
 	last_save_time,
+	bgrewriteaof_in_progress,
 	total_connections_received,
 	total_commands_processed,
 	uptime_in_seconds,
-	uptime_in_days
+	uptime_in_days,
+	hash_max_zipmap_entries,
+	hash_max_zipmap_value,
+	vm_enabled,
+	role
 }
