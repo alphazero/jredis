e66cf2cbac6fe57d2cadffc46a3a68d8dd4bd0cd
Joubin Houshyar <alphazero@sensesay.net>
2012-02-01 14:48:44 -0500

    Cleanup the poms to add version numbers for various plugins -- TESTED
    Note now am using Maven 3.0.4

--- core/ri/src/test/java/org/jredis/ri/alphazero/JRedisProviderTestsBase.java
+++ core/ri/src/test/java/org/jredis/ri/alphazero/JRedisProviderTestsBase.java
@@ -30,7 +30,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import org.jredis.JRedis;
 import org.jredis.ObjectInfo;
